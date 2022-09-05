2703b9c7c23d66e6f82cd660b6c1fd2a1065447b
Kevin Z <zkw917@gmail.com>
2022-09-05 16:04:37 -0700

    Initialize

---
+++ types/deployment.ts
@@ -0,0 +1,18 @@
+export enum TILE_TYPE {
+  COAST = 0,
+  INLAND = 1,
+  WATER = 2,
+  PORT = 3,
+  CITY = 4,
+  OIL_WELL = 5,
+}
+
+export type ComponentSpecStruct = { name: string; valueType: any };
+
+export interface gameConfig {
+  address: string;
+  network: string;
+  deploymentId: string;
+  map: TILE_TYPE[][];
+  componentSpecs: ComponentSpecStruct[];
+}
