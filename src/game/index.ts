3761e028390d9236a689d6bc99d79acdd5f8e6fb
Kevin Z <zkw917@gmail.com>
2022-09-15 13:29:10 -0700

    Fix vault imports

--- src/game/index.ts
+++ src/game/index.ts
@@ -2,4 +2,6 @@ export * from './componentRegistry';
 export * from './componentSerde';
 export * from './types/component';
 
-export * from './typechain-types';
+export * from './typechain-types/hardhat-diamond-abi/Curio';
+export * from './typechain-types/factories/contracts';
+export * from './typechain-types/factories/hardhat-diamond-abi';
