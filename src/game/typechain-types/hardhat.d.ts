bec72861bf1b049a590a5c49130e417e4e863590
Yijia-Chen <yijiachenmain@gmail.com>
2023-01-17 12:21:44 -0800

    feat: contract cleanup

--- src/game/typechain-types/hardhat.d.ts
+++ src/game/typechain-types/hardhat.d.ts
@@ -2,257 +2,410 @@
 /* tslint:disable */
 /* eslint-disable */
 
-import { ethers } from 'ethers';
-import { FactoryOptions, HardhatEthersHelpers as HardhatEthersHelpersBase } from '@nomiclabs/hardhat-ethers/types';
+import { ethers } from "ethers";
+import {
+  FactoryOptions,
+  HardhatEthersHelpers as HardhatEthersHelpersBase,
+} from "@nomiclabs/hardhat-ethers/types";
 
-import * as Contracts from '.';
+import * as Contracts from ".";
 
-declare module 'hardhat/types/runtime' {
+declare module "hardhat/types/runtime" {
   interface HardhatEthersHelpers extends HardhatEthersHelpersBase {
     getContractFactory(
-      name: 'Component',
+      name: "Component",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Component__factory>;
     getContractFactory(
-      name: 'Diamond',
+      name: "Diamond",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Diamond__factory>;
     getContractFactory(
-      name: 'AdminFacet',
+      name: "AdminFacet",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.AdminFacet__factory>;
     getContractFactory(
-      name: 'DiamondCutFacet',
+      name: "DiamondCutFacet",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.DiamondCutFacet__factory>;
     getContractFactory(
-      name: 'DiamondLoupeFacet',
+      name: "DiamondLoupeFacet",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.DiamondLoupeFacet__factory>;
     getContractFactory(
-      name: 'GameFacet',
+      name: "GameFacet",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.GameFacet__factory>;
     getContractFactory(
-      name: 'GetterFacet',
+      name: "GetterFacet",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.GetterFacet__factory>;
     getContractFactory(
-      name: 'OwnershipFacet',
+      name: "OwnershipFacet",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.OwnershipFacet__factory>;
     getContractFactory(
-      name: 'IDiamondCut',
+      name: "IDiamondCut",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IDiamondCut__factory>;
     getContractFactory(
-      name: 'IDiamondLoupe',
+      name: "IDiamondLoupe",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IDiamondLoupe__factory>;
     getContractFactory(
-      name: 'IERC165',
+      name: "IERC165",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IERC165__factory>;
     getContractFactory(
-      name: 'IERC173',
+      name: "IERC173",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IERC173__factory>;
     getContractFactory(
-      name: 'IERC20',
+      name: "IERC20",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IERC20__factory>;
     getContractFactory(
-      name: 'ITreaty',
+      name: "ITreaty",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ITreaty__factory>;
     getContractFactory(
-      name: 'ECSLib',
+      name: "ECSLib",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ECSLib__factory>;
     getContractFactory(
-      name: 'GameLib',
+      name: "GameLib",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.GameLib__factory>;
     getContractFactory(
-      name: 'LibDiamond',
+      name: "LibDiamond",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.LibDiamond__factory>;
     getContractFactory(
-      name: 'UintBoolMapping',
+      name: "UintBoolMapping",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.UintBoolMapping__factory>;
-    getContractFactory(name: 'Set', signerOrOptions?: ethers.Signer | FactoryOptions): Promise<Contracts.Set__factory>;
     getContractFactory(
-      name: 'CurioERC20',
+      name: "Set",
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<Contracts.Set__factory>;
+    getContractFactory(
+      name: "CurioERC20",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.CurioERC20__factory>;
     getContractFactory(
-      name: 'CurioERC721',
+      name: "CurioERC721",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.CurioERC721__factory>;
     getContractFactory(
-      name: 'CurioTreaty',
+      name: "CurioTreaty",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.CurioTreaty__factory>;
     getContractFactory(
-      name: 'CurioWallet',
+      name: "CurioWallet",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.CurioWallet__factory>;
     getContractFactory(
-      name: 'Alliance',
+      name: "Alliance",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Alliance__factory>;
     getContractFactory(
-      name: 'CollectiveDefenseFund',
+      name: "CollectiveDefenseFund",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.CollectiveDefenseFund__factory>;
     getContractFactory(
-      name: 'Embargo',
+      name: "Embargo",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Embargo__factory>;
     getContractFactory(
-      name: 'HandshakeDeal',
+      name: "HandshakeDeal",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.HandshakeDeal__factory>;
     getContractFactory(
-      name: 'NonAggressionPact',
+      name: "NonAggressionPact",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.NonAggressionPact__factory>;
     getContractFactory(
-      name: 'SimpleOTC',
+      name: "SimpleOTC",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.SimpleOTC__factory>;
     getContractFactory(
-      name: 'TestTreaty',
+      name: "TestTreaty",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.TestTreaty__factory>;
     getContractFactory(
-      name: 'AddressComponent',
+      name: "AddressComponent",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.AddressComponent__factory>;
     getContractFactory(
-      name: 'BoolComponent',
+      name: "BoolComponent",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.BoolComponent__factory>;
     getContractFactory(
-      name: 'IntComponent',
+      name: "IntComponent",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.IntComponent__factory>;
     getContractFactory(
-      name: 'PositionComponent',
+      name: "PositionComponent",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.PositionComponent__factory>;
     getContractFactory(
-      name: 'StringComponent',
+      name: "StringComponent",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.StringComponent__factory>;
     getContractFactory(
-      name: 'UintArrayComponent',
+      name: "UintArrayComponent",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.UintArrayComponent__factory>;
     getContractFactory(
-      name: 'UintComponent',
+      name: "UintComponent",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.UintComponent__factory>;
     getContractFactory(
-      name: 'DiamondInit',
+      name: "DiamondInit",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.DiamondInit__factory>;
     getContractFactory(
-      name: 'Curio',
+      name: "Curio",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.Curio__factory>;
     getContractFactory(
-      name: 'ERC20',
+      name: "ERC20",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ERC20__factory>;
     getContractFactory(
-      name: 'ERC721',
+      name: "ERC721",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ERC721__factory>;
     getContractFactory(
-      name: 'ERC721TokenReceiver',
+      name: "ERC721TokenReceiver",
       signerOrOptions?: ethers.Signer | FactoryOptions
     ): Promise<Contracts.ERC721TokenReceiver__factory>;
 
-    getContractAt(name: 'Component', address: string, signer?: ethers.Signer): Promise<Contracts.Component>;
-    getContractAt(name: 'Diamond', address: string, signer?: ethers.Signer): Promise<Contracts.Diamond>;
-    getContractAt(name: 'AdminFacet', address: string, signer?: ethers.Signer): Promise<Contracts.AdminFacet>;
-    getContractAt(name: 'DiamondCutFacet', address: string, signer?: ethers.Signer): Promise<Contracts.DiamondCutFacet>;
     getContractAt(
-      name: 'DiamondLoupeFacet',
+      name: "Component",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Component>;
+    getContractAt(
+      name: "Diamond",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Diamond>;
+    getContractAt(
+      name: "AdminFacet",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.AdminFacet>;
+    getContractAt(
+      name: "DiamondCutFacet",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.DiamondCutFacet>;
+    getContractAt(
+      name: "DiamondLoupeFacet",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.DiamondLoupeFacet>;
-    getContractAt(name: 'GameFacet', address: string, signer?: ethers.Signer): Promise<Contracts.GameFacet>;
-    getContractAt(name: 'GetterFacet', address: string, signer?: ethers.Signer): Promise<Contracts.GetterFacet>;
-    getContractAt(name: 'OwnershipFacet', address: string, signer?: ethers.Signer): Promise<Contracts.OwnershipFacet>;
-    getContractAt(name: 'IDiamondCut', address: string, signer?: ethers.Signer): Promise<Contracts.IDiamondCut>;
-    getContractAt(name: 'IDiamondLoupe', address: string, signer?: ethers.Signer): Promise<Contracts.IDiamondLoupe>;
-    getContractAt(name: 'IERC165', address: string, signer?: ethers.Signer): Promise<Contracts.IERC165>;
-    getContractAt(name: 'IERC173', address: string, signer?: ethers.Signer): Promise<Contracts.IERC173>;
-    getContractAt(name: 'IERC20', address: string, signer?: ethers.Signer): Promise<Contracts.IERC20>;
-    getContractAt(name: 'ITreaty', address: string, signer?: ethers.Signer): Promise<Contracts.ITreaty>;
-    getContractAt(name: 'ECSLib', address: string, signer?: ethers.Signer): Promise<Contracts.ECSLib>;
-    getContractAt(name: 'GameLib', address: string, signer?: ethers.Signer): Promise<Contracts.GameLib>;
-    getContractAt(name: 'LibDiamond', address: string, signer?: ethers.Signer): Promise<Contracts.LibDiamond>;
-    getContractAt(name: 'UintBoolMapping', address: string, signer?: ethers.Signer): Promise<Contracts.UintBoolMapping>;
-    getContractAt(name: 'Set', address: string, signer?: ethers.Signer): Promise<Contracts.Set>;
-    getContractAt(name: 'CurioERC20', address: string, signer?: ethers.Signer): Promise<Contracts.CurioERC20>;
-    getContractAt(name: 'CurioERC721', address: string, signer?: ethers.Signer): Promise<Contracts.CurioERC721>;
-    getContractAt(name: 'CurioTreaty', address: string, signer?: ethers.Signer): Promise<Contracts.CurioTreaty>;
-    getContractAt(name: 'CurioWallet', address: string, signer?: ethers.Signer): Promise<Contracts.CurioWallet>;
-    getContractAt(name: 'Alliance', address: string, signer?: ethers.Signer): Promise<Contracts.Alliance>;
     getContractAt(
-      name: 'CollectiveDefenseFund',
+      name: "GameFacet",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.GameFacet>;
+    getContractAt(
+      name: "GetterFacet",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.GetterFacet>;
+    getContractAt(
+      name: "OwnershipFacet",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.OwnershipFacet>;
+    getContractAt(
+      name: "IDiamondCut",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IDiamondCut>;
+    getContractAt(
+      name: "IDiamondLoupe",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IDiamondLoupe>;
+    getContractAt(
+      name: "IERC165",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC165>;
+    getContractAt(
+      name: "IERC173",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC173>;
+    getContractAt(
+      name: "IERC20",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IERC20>;
+    getContractAt(
+      name: "ITreaty",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ITreaty>;
+    getContractAt(
+      name: "ECSLib",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ECSLib>;
+    getContractAt(
+      name: "GameLib",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.GameLib>;
+    getContractAt(
+      name: "LibDiamond",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.LibDiamond>;
+    getContractAt(
+      name: "UintBoolMapping",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.UintBoolMapping>;
+    getContractAt(
+      name: "Set",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Set>;
+    getContractAt(
+      name: "CurioERC20",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.CurioERC20>;
+    getContractAt(
+      name: "CurioERC721",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.CurioERC721>;
+    getContractAt(
+      name: "CurioTreaty",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.CurioTreaty>;
+    getContractAt(
+      name: "CurioWallet",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.CurioWallet>;
+    getContractAt(
+      name: "Alliance",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Alliance>;
+    getContractAt(
+      name: "CollectiveDefenseFund",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.CollectiveDefenseFund>;
-    getContractAt(name: 'Embargo', address: string, signer?: ethers.Signer): Promise<Contracts.Embargo>;
-    getContractAt(name: 'HandshakeDeal', address: string, signer?: ethers.Signer): Promise<Contracts.HandshakeDeal>;
     getContractAt(
-      name: 'NonAggressionPact',
+      name: "Embargo",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Embargo>;
+    getContractAt(
+      name: "HandshakeDeal",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.HandshakeDeal>;
+    getContractAt(
+      name: "NonAggressionPact",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.NonAggressionPact>;
-    getContractAt(name: 'SimpleOTC', address: string, signer?: ethers.Signer): Promise<Contracts.SimpleOTC>;
-    getContractAt(name: 'TestTreaty', address: string, signer?: ethers.Signer): Promise<Contracts.TestTreaty>;
     getContractAt(
-      name: 'AddressComponent',
+      name: "SimpleOTC",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.SimpleOTC>;
+    getContractAt(
+      name: "TestTreaty",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.TestTreaty>;
+    getContractAt(
+      name: "AddressComponent",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.AddressComponent>;
-    getContractAt(name: 'BoolComponent', address: string, signer?: ethers.Signer): Promise<Contracts.BoolComponent>;
-    getContractAt(name: 'IntComponent', address: string, signer?: ethers.Signer): Promise<Contracts.IntComponent>;
     getContractAt(
-      name: 'PositionComponent',
+      name: "BoolComponent",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.BoolComponent>;
+    getContractAt(
+      name: "IntComponent",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.IntComponent>;
+    getContractAt(
+      name: "PositionComponent",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.PositionComponent>;
-    getContractAt(name: 'StringComponent', address: string, signer?: ethers.Signer): Promise<Contracts.StringComponent>;
     getContractAt(
-      name: 'UintArrayComponent',
+      name: "StringComponent",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.StringComponent>;
+    getContractAt(
+      name: "UintArrayComponent",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.UintArrayComponent>;
-    getContractAt(name: 'UintComponent', address: string, signer?: ethers.Signer): Promise<Contracts.UintComponent>;
-    getContractAt(name: 'DiamondInit', address: string, signer?: ethers.Signer): Promise<Contracts.DiamondInit>;
-    getContractAt(name: 'Curio', address: string, signer?: ethers.Signer): Promise<Contracts.Curio>;
-    getContractAt(name: 'ERC20', address: string, signer?: ethers.Signer): Promise<Contracts.ERC20>;
-    getContractAt(name: 'ERC721', address: string, signer?: ethers.Signer): Promise<Contracts.ERC721>;
     getContractAt(
-      name: 'ERC721TokenReceiver',
+      name: "UintComponent",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.UintComponent>;
+    getContractAt(
+      name: "DiamondInit",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.DiamondInit>;
+    getContractAt(
+      name: "Curio",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.Curio>;
+    getContractAt(
+      name: "ERC20",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ERC20>;
+    getContractAt(
+      name: "ERC721",
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<Contracts.ERC721>;
+    getContractAt(
+      name: "ERC721TokenReceiver",
       address: string,
       signer?: ethers.Signer
     ): Promise<Contracts.ERC721TokenReceiver>;
 
     // default types
-    getContractFactory(name: string, signerOrOptions?: ethers.Signer | FactoryOptions): Promise<ethers.ContractFactory>;
+    getContractFactory(
+      name: string,
+      signerOrOptions?: ethers.Signer | FactoryOptions
+    ): Promise<ethers.ContractFactory>;
     getContractFactory(
       abi: any[],
       bytecode: ethers.utils.BytesLike,
       signer?: ethers.Signer
     ): Promise<ethers.ContractFactory>;
-    getContractAt(nameOrAbi: string | any[], address: string, signer?: ethers.Signer): Promise<ethers.Contract>;
+    getContractAt(
+      nameOrAbi: string | any[],
+      address: string,
+      signer?: ethers.Signer
+    ): Promise<ethers.Contract>;
   }
 }
